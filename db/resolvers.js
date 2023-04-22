a340866bb9b52ccc718035377712893c34d4d589
Gonzalo Hernandez <gonzalo.hernandez1@hotmail.com>
2023-04-22 17:51:32 -0300

    Refactor and continue testing and learning

--- db/resolvers.js
+++ db/resolvers.js
@@ -5,7 +5,7 @@ const courses = [
     },
     {
         title: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-        tech: 'ReactJS',
+        tech: 'React',
     },
     {
         title: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
@@ -22,7 +22,12 @@ const courses = [
 const resolvers = {
     Query: {
         getCourses: () => courses,
-        getTech: ()=> courses
+        getTech: ()=> courses,
+        getCoursesInput: (_, {input}, ctx, info) => {
+            console.log(ctx)
+            const result = courses.filter(c => c.tech === input.tech)
+            return result;
+        }
     }
 }
 
